affa421418f36b04f49ba7d42aa38af79dfad1a8
harry <shsage91@gmail.com>
2022-08-24 22:59:22 +0900

    Setup apollo graphql server with mock data

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,1 +1,16 @@
-console.log("hello!")
+import { ApolloServer } from "apollo-server"
+
+import { typeDefs } from "./schema"
+import { mocks } from "./__mock__"
+
+const main = async () => {
+  const server = new ApolloServer({ typeDefs, mocks })
+  const { port, url } = await server.listen()
+  console.log(`
+    🚀  Server is running!
+    🔉  Listening on port ${port}
+    📭  Query at ${url}
+  `)
+}
+
+void main()
